c523237324d7045cfe7ed71ec0ec8c52d2405de0
Evan Walker <ewalker3764@gmail.com>
2023-05-02 11:21:30 -0400

    users pt2

--- db/users.js
+++ db/users.js
@@ -4,19 +4,65 @@ const client = require("./client");
 
 // user functions
 async function createUser({ username, password }) {
+  try{
+    const { rows: [user] } = await client.query(`
+    INSERT INTO users (username, password)
+    VALUES ($1, $2)
+    ON CONFLICT (username) DO NOTHING
+    RETURNING *;
+    `, [username, password]);
+    return user;
+  } catch (error){
+    throw error;
+  }
   
 }
 
 async function getUser({ username, password }) {
+try {
+  const { rows } = await client.query(`
+  SELECT id, username, password
+  FROM users
+  WHERE username=${username}
+  AND password=${password}
+  `);
 
+  return rows;
+} catch (error) {
+  throw error;
+}
 }
 
 async function getUserById(userId) {
+try {
+  const { rows: [ user ] } = await client.query(`
+  SELECT *
+  FROM users
+  WHERE id=${ userId }
+  `);
+if (user.length === 0) 
+  return;
 
+delete user.password;
+console.log("WHERE IS THE USER!", user)
+return user;
+} catch (error) {
+  throw error;
+}
 }
 
 async function getUserByUsername(userName) {
+  try {
+    const { rows: [user] } = await client.query(`
+      SELECT *
+      FROM users
+      WHERE username=$1;
+    `, [userName]);
 
+    return user;
+} catch (error) {
+  throw error;
+}
 }
 
 module.exports = {
