2df42c2dd674a7bbc59ccf2f365da48ddde8cacf
Evan Walker <ewalker3764@gmail.com>
2023-05-03 13:44:40 -0400

    new stuff

--- db/routine_activities.js
+++ db/routine_activities.js
@@ -7,18 +7,19 @@ async function addActivityToRoutine({
   duration,
 }) { 
   try {
-  const {rows: [routines]} = await client.query(`
-  INSERT INTO routines ("routineId", "activityId", count, duration)
-  VALUES ($1, $2, $3, $4)
-  RETURNING *;
-  `, [routineId, activityId, count, duration]);
-  const routActivity = await createActivity(routine_activities);
-  return routActivity;
-} catch (error) {
-  throw error;
+    const { rows: [routine_activity] } = await client.query(`
+      INSERT INTO routine_activity ("routineId", "activityId", count, duration)
+      VALUES ($1, $2, $3, $4)
+      RETURNING *;
+    `, [routineId, activityId, count, duration]);
+    
+    return routine_activity;
+  } catch (error) {
+    throw error;
+  }
+  
 }
 
-}
 
 async function getRoutineActivityById(id) {
   try {
