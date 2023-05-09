85735081a30121c1074b97d3773a1d43bc105128
bday <brookeharris2248@gmail.com>
2023-05-09 13:35:26 -0400

    the stuff

--- FRONTEND/src/api/index.js
+++ FRONTEND/src/api/index.js
@@ -1,31 +1,194 @@
 // BASE
+const BASE = 'http://fitnesstrac-kr.herokuapp.com/api/'
 
 // REGISTER USER
-
+export const registerUser = async (userObject) => {
+    try {
+      const response = await fetch(`${BASE}/users/register`,
+        {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify(userObject),
+        }
+      );
+  
+      const { success, error, data } = await response.json();
+  
+      if (success) {
+        const { token, message } = data;
+        localStorage.setItem('token', token);
+        return { token, message };
+      }
+      if (!success && !error) {
+        const { name, message } = data;
+        return { name, message };
+      }
+      console.log(success, error, data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
 //LOGIN
-
+export const loginUser = async (userObject) => {
+    try {
+      const response = await fetch(`${BASE}/users/login`,
+        {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify(userObject),
+        }
+      );
+  
+      const { success, error, data } = await response.json();
+  
+      if (success) {
+        const { token, message } = data;
+        localStorage.setItem('token', token);
+        return { token, message };
+      }
+      if (!success && !error) {
+        const { name, message } = data;
+        return { name, message };
+      }
+      console.log(success, error, data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
 //USER- GET ME
+export const getMe = async (token) => {
 
-//USER ROUTINES
+    try {
+      const response = await fetch(`${BASE}/users/me`, {
+          method: 'GET',
+          headers: {
+          'Content-Type': 'application/json',
+          'Authorization': `Bearer ${token}`
+        },
+      });
+      const result = await response.json();
+      console.log(result);
+      return result
+    } catch (err) {
+      console.error(err);
+    }
+  }
+//GET ROUTINES BY USER
 
 //GET ALL ACTIVITIES
-
+export async function getAllActivities() {
+    try {
+      const response = await fetch(`${BASE}/routines`);
+      const activitiesList = await response.json();
+      console.log(activitiesList.data.activities);
+      return activitiesList.data.activities;
+    } catch (error) {
+      throw error;
+    }
+  }
 //CREATE ACTIVITY
+export const createActivity = async (newActivityObj, token) => {
+ 
+    try {
+      
+      const response = await fetch(`${BASE}/activities`, {
+        method: "POST",
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': `Bearer ${token}`
+        },
+        body: JSON.stringify(newActivityObj),
+      });
+      console.log(response);
+      const result = await response.json();
+      console.log("The result of addNewPost is: ",result);
+     
+    return result;
+    } catch (err) {
+      console.error(err);
+    }
+  }
 
-//EDIT ACTIVITY
 
 //GET AVTIVITIES BY ROUTINE
 
 // GET ALL ROUTINES
-
+export async function getAllRoutines() {
+    try {
+      const response = await fetch(`${BASE}/routines`);
+      const routinesList = await response.json();
+      console.log(routinesList.data.routines);
+      return routinesList.data.routines;
+    } catch (error) {
+      throw error;
+    }
+  }
 //CREATE ROUTINES
-
-//EDIT ROUTINES
+export const createRoutine = async (newRoutineObj, token) => {
+ 
+    try {
+      
+      const response = await fetch(`${BASE}/routines`, {
+        method: "POST",
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': `Bearer ${token}`
+        },
+        body: JSON.stringify(newRoutineObj),
+      });
+      console.log(response);
+      const result = await response.json();
+      console.log("The result of addNewPost is: ",result);
+     
+    return result;
+    } catch (err) {
+      console.error(err);
+    }
+  }
 
 //DELETE ROUTINES
+export const deleteRoutine = async (id, token) => {
 
-// ATTACHES ACTIVITY TO ROUTINE
+    try {
+      
+      const response = await fetch(`${BASE}/routines/${id}`, {
+        method: 'DELETE',
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': `Bearer ${token}`
+        },
+      });
+  
+      const result = await response.json();
+      console.log(result);
+      return result;
+    } catch (error) {
+      console.error(error);
+    }
+  };
 
-//EDIT ROUTINE ACTIVITY
 
 //DELETE ROUTINE ACTIVITY
+export const deleteRoutineActivity = async (id, token) => {
+
+    try {
+      
+      const response = await fetch(`${BASE}/routineActivities/${id}`, {
+        method: 'DELETE',
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': `Bearer ${token}`
+        },
+      });
+  
+      const result = await response.json();
+      console.log(result);
+      return result;
+    } catch (error) {
+      console.error(error);
+    }
+  };
